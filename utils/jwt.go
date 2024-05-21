07141af85224a32d10b2bb6b8bfc37205f127d54
chico <chico.jaguk@gmail.com>
2024-05-21 11:28:54 +0700

    fix get secret key

--- utils/jwt.go
+++ utils/jwt.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"log"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
@@ -13,6 +15,7 @@ func ExtractJWT(c *fiber.Ctx) (string, error) {
 
 	cookie := c.Cookies("jwt")
 	SecretKey := []byte(os.Getenv("JWT_PRIVATE_KEY"))
+
 	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(SecretKey), nil
 	})
@@ -24,4 +27,20 @@ func ExtractJWT(c *fiber.Ctx) (string, error) {
 	}
 	claims := token.Claims.(*jwt.StandardClaims)
 	return claims.Issuer, nil
+}
+
+func GenerateJWT(id uint, tokenTTL int) (string, error) {
+	SecretKey := []byte(os.Getenv("JWT_PRIVATE_KEY"))
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    strconv.Itoa(int(id)),
+		ExpiresAt: time.Now().Add(time.Second * time.Duration(tokenTTL)).Unix(),
+	})
+
+	token, err := claims.SignedString([]byte(SecretKey))
+
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
 }
