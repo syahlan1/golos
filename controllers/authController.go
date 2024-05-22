67c6b6a94af8dd5048f644f65ae81f1b4d684142
chico <chico.jaguk@gmail.com>
2024-05-22 09:11:16 +0700

    add correct response in auth controller

--- controllers/authController.go
+++ controllers/authController.go
@@ -19,21 +19,27 @@ func Register(c *fiber.Ctx) error {
 	var data map[string]string
 
 	if err := c.BodyParser(&data); err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error()})
 	}
 
 	// Check apakah username sudah ada
 	var existingUser models.Users
 	if err := connection.DB.Where("username = ?", data["username"]).First(&existingUser).Error; err == nil {
-		return c.Status(fiber.StatusConflict).JSON(fiber.Map{
-			"message": "Username already exists",
+		return c.Status(fiber.StatusConflict).JSON(models.Response{
+			Code:    fiber.StatusConflict,
+			Message: "Username already exists",
 		})
 	}
 
 	// Generate hashed password
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(data["password"]), bcrypt.DefaultCost)
 	if err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
 	// Create new user entry
@@ -45,18 +51,27 @@ func Register(c *fiber.Ctx) error {
 		RoleId:   1,
 	}
 	if err := connection.DB.Create(&newUser).Error; err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to create user"})
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: "Failed to create user",
+		})
 	}
 
 	// Return success response
-	return c.JSON(fiber.Map{"message": "User registered successfully"})
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "User registered successfully",
+	})
 }
 
 func Login(c *fiber.Ctx) error {
 	var data map[string]string
 
 	if err := c.BodyParser(&data); err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
 	var user models.Users
@@ -65,22 +80,25 @@ func Login(c *fiber.Ctx) error {
 
 	if user.Id == 0 {
 		c.Status(fiber.StatusNotFound)
-		return c.JSON(fiber.Map{
-			"message": "User not found",
+		return c.JSON(models.Response{
+			Code:    fiber.StatusNotFound,
+			Message: "User not found",
 		})
 	}
 
 	if user.IsLogin == 1 {
 		c.Status(fiber.StatusUnauthorized)
-		return c.JSON(fiber.Map{
-			"message": "User is already active",
+		return c.JSON(models.Response{
+			Code:    fiber.StatusUnauthorized,
+			Message: "User is already active",
 		})
 	}
 
 	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
 		c.Status(fiber.StatusBadRequest)
-		return c.JSON(fiber.Map{
-			"message": "incorrect password",
+		return c.JSON(models.Response{
+			Code:    fiber.StatusBadRequest,
+			Message: "incorrect password",
 		})
 	}
 
@@ -90,8 +108,9 @@ func Login(c *fiber.Ctx) error {
 	tokenTTL, err := strconv.Atoi(param.ParamValue)
 
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Invalid token TTL value",
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: "Invalid token TTL value",
 		})
 	}
 
@@ -99,8 +118,9 @@ func Login(c *fiber.Ctx) error {
 
 	if err != nil {
 		c.Status(fiber.StatusInternalServerError)
-		return c.JSON(fiber.Map{
-			"message": "could't create token",
+		return c.JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: "could't create token",
 		})
 	}
 
@@ -116,8 +136,9 @@ func Login(c *fiber.Ctx) error {
 
 	if err := connection.DB.Model(&user).Update("is_login", 1).Error; err != nil {
 		c.Status(fiber.StatusInternalServerError)
-		return c.JSON(fiber.Map{
-			"message": "Failed to update user status"})
+		return c.JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: "Failed to update user status"})
 	}
 
 	time.AfterFunc(time.Second*time.Duration(tokenTTL), func() {
@@ -141,42 +162,73 @@ func User(c *fiber.Ctx) error {
 	// Mendapatkan data pengguna (user) dari database
 	var user models.Users
 	if err := connection.DB.Where("id = ?", claims).Preload("Role").First(&user).Error; err != nil {
-		return err
+		return c.Status(fiber.StatusNotFound).JSON(models.Response{
+			Code:    fiber.StatusNotFound,
+			Message: err.Error(),
+		})
 	}
 
 	// Preload izin-izin (permissions) dari peran (role) pengguna
 	if err := connection.DB.Model(&user.Role).Association("Permissions").Find(&user.Role.Permissions); err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
-	return c.JSON(user)
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Success",
+		Data:    user,
+	})
 }
 
 func ShowRole(c *fiber.Ctx) error {
 	var roles []models.Roles
 	if err := connection.DB.Find(&roles).Error; err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
-	return c.JSON(roles)
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Success",
+		Data:    roles,
+	})
 }
 
 func ShowAllPermissions(c *fiber.Ctx) error {
 	var permissions []models.Permission
 	if err := connection.DB.Find(&permissions).Error; err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
-	return c.JSON(permissions)
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Success",
+		Data:    permissions,
+	})
 }
 
 func ShowPermissions(c *fiber.Ctx) error {
 	roleID := c.Params("id")
 	var role models.Roles
 	if err := connection.DB.Preload("Permissions").Where("id = ?", roleID).First(&role).Error; err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
-	return c.JSON(role)
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Success",
+		Data:    role,
+	})
 }
 
 // logout
@@ -196,8 +248,9 @@ func Logout(c *fiber.Ctx) error {
 
 	connection.DB.Model(&user).Where("id = ?", userId).Update("is_login", 0)
 
-	return c.JSON(fiber.Map{
-		"message": "Logged out Successfully!",
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Logged out Successfully!",
 	})
 }
 
@@ -223,27 +276,39 @@ func CreateRole(c *fiber.Ctx) error {
 		Permissions []string `json:"permissions"`
 	}
 	if err := c.BodyParser(&req); err != nil {
-		return err
+		return c.Status(fiber.StatusBadRequest).JSON(models.Response{
+			Code:    fiber.StatusBadRequest,
+			Message: err.Error(),
+		})
 	}
 
 	// Get user role ID
 	claims, err := utils.ExtractJWT(c)
 	if err != nil {
 		c.Status(fiber.StatusUnauthorized)
-		return errors.New("status unauthorized")
+		return c.Status(fiber.StatusUnauthorized).JSON(models.Response{
+			Code:    fiber.StatusUnauthorized,
+			Message: "Unauthorized",
+		})
 	}
 
 	var user models.Users
 	if err := connection.DB.Where("id = ?", claims).First(&user).Error; err != nil {
 		log.Println("Error retrieving user:", err)
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
 	// Cek apakah role dengan nama yang sama sudah ada
 	var existingRole models.Roles
 	if err := connection.DB.Where("name = ?", req.Name).First(&existingRole).Error; err == nil {
 		// Role sudah ada, kirim respons konflik
-		return c.Status(fiber.StatusConflict).JSON(fiber.Map{"message": "Role already exists"})
+		return c.Status(fiber.StatusConflict).JSON(models.Response{
+			Code:    fiber.StatusConflict,
+			Message: "Role already exists",
+		})
 	}
 
 	// Buat role baru
@@ -251,13 +316,19 @@ func CreateRole(c *fiber.Ctx) error {
 
 	// Simpan role baru ke dalam database
 	if err := connection.DB.Create(&newRole).Error; err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
 	// Dapatkan ID permissions berdasarkan nama permissions yang diberikan
 	var permissions []models.Permission
 	if err := connection.DB.Where("name IN ?", req.Permissions).Find(&permissions).Error; err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
 	// Buat entri RolePermission untuk setiap permission yang terkait dengan role baru
@@ -268,12 +339,18 @@ func CreateRole(c *fiber.Ctx) error {
 			// RolePermission belum ada, tambahkan entri baru
 			rolePermission := models.RolePermission{RolesId: newRole.Id, PermissionId: permission.Id}
 			if err := connection.DB.Create(&rolePermission).Error; err != nil {
-				return err
+				return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+					Code:    fiber.StatusInternalServerError,
+					Message: err.Error(),
+				})
 			}
 		}
 	}
 
-	return c.JSON(fiber.Map{"message": "Role created successfully"})
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Role created successfully",
+	})
 }
 
 func DeleteRole(c *fiber.Ctx) error {
@@ -285,28 +362,43 @@ func DeleteRole(c *fiber.Ctx) error {
 	// Hapus role permission yang terkait dengan role yang akan dihapus
 	if err := tx.Where("roles_id = ?", roleID).Delete(&models.RolePermission{}).Error; err != nil {
 		tx.Rollback()
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
 	// Update role_id user menjadi 0 untuk user yang memiliki role yang akan dihapus
 	if err := tx.Model(&models.Users{}).Where("role_id = ?", roleID).Update("role_id", 0).Error; err != nil {
 		tx.Rollback()
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
 	// Hapus role
 	if err := tx.Where("id = ?", roleID).Delete(&models.Roles{}).Error; err != nil {
 		tx.Rollback()
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
 	// Commit transaksi
 	if err := tx.Commit().Error; err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
 	// Jika sukses, kirim respons sukses
-	return c.JSON(fiber.Map{"message": "Role deleted successfully"})
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Role deleted successfully",
+	})
 }
 
 func UpdateRole(c *fiber.Ctx) error {
@@ -317,7 +409,10 @@ func UpdateRole(c *fiber.Ctx) error {
 		Permissions []string `json:"permissions"`
 	}
 	if err := c.BodyParser(&req); err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
 	// Get user role ID
@@ -329,7 +424,9 @@ func UpdateRole(c *fiber.Ctx) error {
 	var user models.Users
 	if err := connection.DB.Where("id = ?", claims).First(&user).Error; err != nil {
 		log.Println("Error retrieving user:", err)
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			
+		})
 	}
 
 	// Get role ID from URL parameter
@@ -338,7 +435,7 @@ func UpdateRole(c *fiber.Ctx) error {
 	// Find existing role by ID
 	var existingRole models.Roles
 	if err := connection.DB.Where("id = ?", roleID).First(&existingRole).Error; err != nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message": "Role not found"})
+		return c.Status(fiber.StatusNotFound).JSON(models.Response{Message: "Role not found"})
 	}
 
 	// Update role name
@@ -348,13 +445,19 @@ func UpdateRole(c *fiber.Ctx) error {
 
 	// Save the updated role to database
 	if err := connection.DB.Save(&existingRole).Error; err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
 	// Get IDs of permissions from the input
 	var permissions []models.Permission
 	if err := connection.DB.Where("name IN ?", req.Permissions).Find(&permissions).Error; err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
 	// Collect IDs of permissions from the input
@@ -373,15 +476,21 @@ func UpdateRole(c *fiber.Ctx) error {
 			// RolePermission doesn't exist, create a new one
 			rolePermission := models.RolePermission{RolesId: existingRole.Id, PermissionId: permission.Id}
 			if err := connection.DB.Create(&rolePermission).Error; err != nil {
-				return err
+				return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+					Code:    fiber.StatusInternalServerError,
+					Message: err.Error(),
+				})
 			}
 		}
 	}
 
 	// Delete existing RolePermission entries not present in req.Permissions
 	if err := connection.DB.Where("roles_id = ? AND permission_id NOT IN ?", existingRole.Id, permissionIDs).Delete(&models.RolePermission{}).Error; err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
 
-	return c.JSON(fiber.Map{"message": "Role updated successfully"})
+	return c.JSON(models.Response{Message: "Role updated successfully"})
 }
