41b41b7beb28d890b63618284141170366506dab
chico <chico.jaguk@gmail.com>
2024-05-28 10:41:23 +0700

    organize ownership data controller

--- controllers/ownershipDataController/ownershipDataController.go
+++ controllers/ownershipDataController/ownershipDataController.go
@@ -4,133 +4,84 @@ import (
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/syahlan1/golos/connection"
 	"github.com/syahlan1/golos/models"
-	"github.com/syahlan1/golos/utils"
+	"github.com/syahlan1/golos/services/ownershipDataService"
 )
 
 func CreateOwnershipData(c *fiber.Ctx) error {
-	var data map[string]interface{}
+	var data models.CreateOwnershipData
 
 	if err := c.BodyParser(&data); err != nil {
-		return err
+		return c.Status(fiber.StatusBadRequest).JSON(models.Response{
+			Code:    fiber.StatusBadRequest,
+			Message: err.Error(),
+		})
 	}
 
-	ownership := models.OwnershipData{
-		Name:              utils.GetStringValue(data, "name"),
-		NoIdentity:        utils.GetStringValue(data, "no_identity"),
-		IdCardAddress:     utils.GetStringValue(data, "id_card_identity"),
-		City:              utils.GetStringValue(data, "city"),
-		ZipCode:           utils.GetStringValue(data, "zip_code"),
-		HomeOwnership:     utils.GetStringValue(data, "home_ownership"),
-		Remark:            utils.GetStringValue(data, "remark"),
-		CifManager:        utils.GetStringValue(data, "cif_manager"),
-		BirthDate:         utils.GetStringValue(data, "birth_date"),
-		LastEducation:     utils.GetStringValue(data, "last_education"),
-		NPWP:              utils.GetStringValue(data, "npwp"),
-		JobTitle:          utils.GetStringValue(data, "job_title"),
-		Experince:         utils.GetStringValue(data, "experience"),
-		OwnershipMarket:   utils.GetIntValue(data, "ownership_market"),
-		CitizenshipStatus: utils.GetStringValue(data, "citizenship_status"),
-		Gender:            utils.GetStringValue(data, "gender"),
-		MaritalStatus:     utils.GetStringValue(data, "marital_status"),
-		NumberOfChildren:  utils.GetIntValue(data, "number_of_children"),
-		StartDate:         utils.GetStringValue(data, "start_date"),
-		KeyPerson:         utils.GetStringValue(data, "key_person"),
-		Removed:           utils.GetStringValue(data, "removed"),
-		Status:            "L",
+	err := ownershipDataService.CreateOwnershipData(data)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(models.Response{
+			Code:    fiber.StatusBadRequest,
+			Message: err.Error(),
+		})
 	}
 
-	if err := connection.DB.Create(&ownership).Error; err != nil {
-		return err
-	}
-
-	return c.JSON(fiber.Map{
-		"message": "insert sukses",
+	return c.JSON(models.Response{
+		Code:    fiber.StatusCreated,
+		Message: "insert sukses",
 	})
 }
 
 func ShowOwnershipData(c *fiber.Ctx) error {
-	var ownershipData []models.OwnershipData
+	result := ownershipDataService.ShowOwnershipData()
 
-	connection.DB.Where("status = ?", "L").Find(&ownershipData)
-
-	return c.JSON(ownershipData)
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Success",
+		Data:    result,
+	})
 }
 
 func EditOwnershipData(c *fiber.Ctx) error {
 	id := c.Params("id")
 
-	var ownershipData models.OwnershipData
-	if err := connection.DB.First(&ownershipData, id).Error; err != nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"status": "Data Not Found",
-		})
-	}
-
-	var updatedOwnershipData models.OwnershipData
-	if err := c.BodyParser(&updatedOwnershipData); err != nil {
+	var data models.OwnershipData
+	if err := c.BodyParser(&data); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"status": "Invalid Data",
 		})
 	}
 
-	ownershipData.Name = updatedOwnershipData.Name
-	ownershipData.NoIdentity = updatedOwnershipData.NoIdentity
-	ownershipData.IdCardAddress = updatedOwnershipData.IdCardAddress
-	ownershipData.City = updatedOwnershipData.City
-	ownershipData.ZipCode = updatedOwnershipData.ZipCode
-	ownershipData.HomeOwnership = updatedOwnershipData.HomeOwnership
-	ownershipData.Remark = updatedOwnershipData.Remark
-	ownershipData.CifManager = updatedOwnershipData.CifManager
-	ownershipData.BirthDate = updatedOwnershipData.BirthDate
-	ownershipData.LastEducation = updatedOwnershipData.LastEducation
-	ownershipData.NPWP = updatedOwnershipData.NPWP
-	ownershipData.JobTitle = updatedOwnershipData.JobTitle
-	ownershipData.Experince = updatedOwnershipData.Experince
-	ownershipData.OwnershipMarket = updatedOwnershipData.OwnershipMarket
-	ownershipData.CitizenshipStatus = updatedOwnershipData.CitizenshipStatus
-	ownershipData.Gender = updatedOwnershipData.Gender
-	ownershipData.MaritalStatus = updatedOwnershipData.MaritalStatus
-	ownershipData.NumberOfChildren = updatedOwnershipData.NumberOfChildren
-	ownershipData.StartDate = updatedOwnershipData.StartDate
-	ownershipData.KeyPerson = updatedOwnershipData.KeyPerson
-	ownershipData.Removed = updatedOwnershipData.Removed
-	ownershipData.Status = updatedOwnershipData.Status
-
-	if err := connection.DB.Save(&ownershipData).Error; err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"status": "Failed to update the user data",
+	result, err := ownershipDataService.EditOwnershipData(id, data)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(models.Response{
+			Code:    fiber.StatusBadRequest,
+			Message: err.Error(),
 		})
 	}
 
-	return c.JSON(fiber.Map{
-		"data":   ownershipData,
-		"status": "Updated!",
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Success",
+		Data:    result,
 	})
 }
 
 func DeleteOwnershipData(c *fiber.Ctx) error {
 	ownershipId := c.Params("id")
 
-	var ownership models.OwnershipData
-	if err := connection.DB.First(&ownership, ownershipId).Error; err != nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"status": "Data Not Found",
+	result, err := ownershipDataService.DeleteOwnershipData(ownershipId)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(models.Response{
+			Code:    fiber.StatusBadRequest,
+			Message: err.Error(),
 		})
 	}
 
-	ownership.Status = "D"
-
-	if err := connection.DB.Save(&ownership).Error; err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"status": "Failed to delete",
-		})
-	}
-
-	return c.JSON(fiber.Map{
-		"data":   ownership,
-		"status": "Deleted!",
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Deleted",
+		Data:    result,
 	})
 
 }
@@ -143,136 +94,94 @@ func CreateRelationWithBank(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid ownership ID"})
 	}
 
-	var data map[string]interface{}
+	var data models.CreateRelationWithBank
 
 	if err := c.BodyParser(&data); err != nil {
-		return err
+		return c.Status(fiber.StatusBadRequest).JSON(models.Response{
+			Code:    fiber.StatusBadRequest,
+			Message: err.Error(),
+		})
 	}
 
-	relation := models.RelationWithBank{
-		Giro:            utils.GetStringValue(data, "giro"),
-		Tabungan:        utils.GetStringValue(data, "tabungan"),
-		NoRekening:      utils.GetIntValue(data, "no_rekening"),
-		Debitur:         utils.GetStringValue(data, "debitur"),
-		Status:          "L",
-		OwnershipDataId: ownershipIdInt,
+	err = ownershipDataService.CreateRelationWithBank(ownershipIdInt, data)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(models.Response{
+			Code:    fiber.StatusBadRequest,
+			Message: err.Error(),
+		})
 	}
 
-	debitur := models.DataRekeningDebitur{
-		Name:            utils.GetStringValue(data, "name"),
-		NoIdCard:        utils.GetStringValue(data, "no_id_card"),
-		NPWP:            utils.GetIntValue(data, "npwp"),
-		KeyPerson:       utils.GetStringValue(data, "key_person"),
-		NoRekening:      utils.GetIntValue(data, "no_rekening"),
-		Remark:          utils.GetStringValue(data, "remark"),
-		Status:          "L",
-		OwnershipDataId: ownershipIdInt,
-	}
-
-	if err := connection.DB.Create(&relation).Error; err != nil {
-		return err
-	}
-
-	if err := connection.DB.Create(&debitur).Error; err != nil {
-		return err
-	}
-
-	return c.JSON(fiber.Map{
-		"message": "insert sukses",
+	return c.JSON(models.Response{
+		Code:    fiber.StatusCreated,
+		Message: "insert sukses",
 	})
 }
 
 func UpdateRekeningDebitur(c *fiber.Ctx) error {
 	id := c.Params("id")
 
-	var rekeningDebitur models.DataRekeningDebitur
-	if err := connection.DB.First(&rekeningDebitur, id).Error; err != nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"status": "Data Not Found",
-		})
-	}
-
-	var updatedRekeningDebitur models.DataRekeningDebitur
-	if err := c.BodyParser(&rekeningDebitur); err != nil {
+	var data models.DataRekeningDebitur
+	if err := c.BodyParser(&data); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"status": "Invalid Data",
 		})
 	}
 
-	rekeningDebitur.Name = updatedRekeningDebitur.Name
-	rekeningDebitur.NoIdCard = updatedRekeningDebitur.NoIdCard
-	rekeningDebitur.NPWP = updatedRekeningDebitur.NPWP
-	rekeningDebitur.KeyPerson = updatedRekeningDebitur.KeyPerson
-	rekeningDebitur.NoRekening = updatedRekeningDebitur.NoRekening
-	rekeningDebitur.Remark = updatedRekeningDebitur.Remark
-
-	if err := connection.DB.Save(&rekeningDebitur).Error; err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"status": "Failed to update the user data",
+	result, err := ownershipDataService.UpdateRekeningDebitur(id, data)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(models.Response{
+			Code:    fiber.StatusBadRequest,
+			Message: err.Error(),
 		})
 	}
 
-	return c.JSON(fiber.Map{
-		"data":   rekeningDebitur,
-		"status": "Updated!",
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Updated!",
+		Data:    result,
 	})
 }
 
 func UpdateRelationWithBank(c *fiber.Ctx) error {
 	id := c.Params("id")
 
-	var relationBank models.RelationWithBank
-	if err := connection.DB.First(&relationBank, id).Error; err != nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"status": "Data Not Found",
-		})
-	}
-
-	var updateRelationBank models.RelationWithBank
-	if err := c.BodyParser(&updateRelationBank); err != nil {
+	var data models.RelationWithBank
+	if err := c.BodyParser(&data); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"status": "Invalid Data",
 		})
 	}
 
-	relationBank.Giro = updateRelationBank.Giro
-	relationBank.Tabungan = updateRelationBank.Tabungan
-	relationBank.NoRekening = updateRelationBank.NoRekening
-	relationBank.Debitur = updateRelationBank.Debitur
-
-	if err := connection.DB.Save(&relationBank).Error; err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"status": "Failed to update the user data",
+	result, err := ownershipDataService.UpdateRelationWithBank(id, data)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(models.Response{
+			Code:    fiber.StatusBadRequest,
+			Message: err.Error(),
 		})
 	}
 
-	return c.JSON(fiber.Map{
-		"data":   relationBank,
-		"status": "Updated!",
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Updated!",
+		Data:    result,
 	})
 }
 
 func DeleteRelationWithBank(c *fiber.Ctx) error {
 	Id := c.Params("id")
 
-	var relation models.RelationWithBank
-	if err := connection.DB.First(&relation, Id).Error; err != nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"status": "Data Not Found",
+	result, err := ownershipDataService.DeleteRelationWithBank(Id)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(models.Response{
+			Code:    fiber.StatusBadRequest,
+			Message: err.Error(),
 		})
 	}
 
-	relation.Status = "D"
-
-	if err := connection.DB.Save(&relation).Error; err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"status": "Failed to delete",
-		})
-	}
-
-	return c.JSON(fiber.Map{
-		"data":   relation,
-		"status": "Deleted!",
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Deleted!",
+		Data:    result,
 	})
 
 }
@@ -280,40 +189,38 @@ func DeleteRelationWithBank(c *fiber.Ctx) error {
 func DeleteRekeningDebitur(c *fiber.Ctx) error {
 	Id := c.Params("id")
 
-	var rekeningDebitur models.DataRekeningDebitur
-	if err := connection.DB.First(&rekeningDebitur, Id).Error; err != nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"status": "Data Not Found",
+	result, err := ownershipDataService.DeleteRekeningDebitur(Id)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(models.Response{
+			Code:    fiber.StatusBadRequest,
+			Message: err.Error(),
 		})
 	}
 
-	rekeningDebitur.Status = "D"
-
-	if err := connection.DB.Save(&rekeningDebitur).Error; err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"status": "Failed to delete",
-		})
-	}
-
-	return c.JSON(fiber.Map{
-		"data":   rekeningDebitur,
-		"status": "Deleted!",
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Deleted!",
+		Data:    result,
 	})
 
 }
 
 func ShowRelationWithBank(c *fiber.Ctx) error {
-	var relationBank []models.RelationWithBank
+	result := ownershipDataService.ShowRelationWithBank()
 
-	connection.DB.Where("status = ?", "L").Find(&relationBank)
-
-	return c.JSON(relationBank)
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Success",
+		Data:    result,
+	})
 }
 
 func ShowRekeningDebitur(c *fiber.Ctx) error {
-	var rekeningDebitur []models.OwnershipData
+	result := ownershipDataService.ShowRekeningDebitur()
 
-	connection.DB.Where("status = ?", "L").Find(&rekeningDebitur)
-
-	return c.JSON(rekeningDebitur)
+	return c.JSON(models.Response{
+		Code:    fiber.StatusOK,
+		Message: "Success",
+		Data:    result,
+	})
 }
--- services/ownershipDataService/ownershipData.go
+++ services/ownershipDataService/ownershipData.go
@@ -1,1 +1,215 @@
 package ownershipDataService
+
+import (
+	"errors"
+
+	"github.com/syahlan1/golos/connection"
+	"github.com/syahlan1/golos/models"
+)
+
+func CreateOwnershipData(data models.CreateOwnershipData) (err error) {
+	ownership := models.OwnershipData{
+		Name:              data.Name,
+		NoIdentity:        data.NoIdentity,
+		IdCardAddress:     data.IdCardAddress,
+		City:              data.City,
+		ZipCode:           data.ZipCode,
+		HomeOwnership:     data.HomeOwnership,
+		Remark:            data.Remark,
+		CifManager:        data.CifManager,
+		BirthDate:         data.BirthDate,
+		LastEducation:     data.LastEducation,
+		NPWP:              data.NPWP,
+		JobTitle:          data.JobTitle,
+		Experince:         data.Experince,
+		OwnershipMarket:   data.OwnershipMarket,
+		CitizenshipStatus: data.CitizenshipStatus,
+		Gender:            data.Gender,
+		MaritalStatus:     data.MaritalStatus,
+		NumberOfChildren:  data.NumberOfChildren,
+		StartDate:         data.StartDate,
+		KeyPerson:         data.KeyPerson,
+		Removed:           data.Removed,
+		Status:            "L",
+	}
+
+	if err := connection.DB.Create(&ownership).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ShowOwnershipData() (result []models.OwnershipData) {
+	var ownershipData []models.OwnershipData
+
+	connection.DB.Where("status = ?", "L").Find(&ownershipData)
+	return ownershipData
+}
+
+func EditOwnershipData(id string, updatedOwnershipData models.OwnershipData) (result models.OwnershipData, err error) {
+	var ownershipData models.OwnershipData
+	if err := connection.DB.First(&ownershipData, id).Error; err != nil {
+		return result, errors.New("data Not Found")
+	}
+
+	ownershipData.Name = updatedOwnershipData.Name
+	ownershipData.NoIdentity = updatedOwnershipData.NoIdentity
+	ownershipData.IdCardAddress = updatedOwnershipData.IdCardAddress
+	ownershipData.City = updatedOwnershipData.City
+	ownershipData.ZipCode = updatedOwnershipData.ZipCode
+	ownershipData.HomeOwnership = updatedOwnershipData.HomeOwnership
+	ownershipData.Remark = updatedOwnershipData.Remark
+	ownershipData.CifManager = updatedOwnershipData.CifManager
+	ownershipData.BirthDate = updatedOwnershipData.BirthDate
+	ownershipData.LastEducation = updatedOwnershipData.LastEducation
+	ownershipData.NPWP = updatedOwnershipData.NPWP
+	ownershipData.JobTitle = updatedOwnershipData.JobTitle
+	ownershipData.Experince = updatedOwnershipData.Experince
+	ownershipData.OwnershipMarket = updatedOwnershipData.OwnershipMarket
+	ownershipData.CitizenshipStatus = updatedOwnershipData.CitizenshipStatus
+	ownershipData.Gender = updatedOwnershipData.Gender
+	ownershipData.MaritalStatus = updatedOwnershipData.MaritalStatus
+	ownershipData.NumberOfChildren = updatedOwnershipData.NumberOfChildren
+	ownershipData.StartDate = updatedOwnershipData.StartDate
+	ownershipData.KeyPerson = updatedOwnershipData.KeyPerson
+	ownershipData.Removed = updatedOwnershipData.Removed
+	ownershipData.Status = updatedOwnershipData.Status
+
+	if err := connection.DB.Save(&ownershipData).Error; err != nil {
+		return result, errors.New("failed to update the user data")
+	}
+
+	return ownershipData, nil
+}
+
+func DeleteOwnershipData(ownershipId string) (result models.OwnershipData, err error) {
+	var ownership models.OwnershipData
+	if err := connection.DB.First(&ownership, ownershipId).Error; err != nil {
+		return result, errors.New("data Not Found")
+	}
+
+	ownership.Status = "D"
+
+	if err := connection.DB.Save(&ownership).Error; err != nil {
+		return result, errors.New("failed to delete")
+	}
+
+	return ownership, nil
+}
+
+func CreateRelationWithBank(ownershipIdInt int, data models.CreateRelationWithBank) (err error) {
+	relation := models.RelationWithBank{
+		Giro:            data.Giro,
+		Tabungan:        data.Tabungan,
+		NoRekening:      data.NoRekening,
+		Debitur:         data.Debitur,
+		Status:          "L",
+		OwnershipDataId: ownershipIdInt,
+	}
+
+	debitur := models.DataRekeningDebitur{
+		Name:            data.Name,
+		NoIdCard:        data.NoIdCard,
+		NPWP:            data.NPWP,
+		KeyPerson:       data.KeyPerson,
+		NoRekening:      data.NoRekening,
+		Remark:          data.Remark,
+		Status:          "L",
+		OwnershipDataId: ownershipIdInt,
+	}
+
+	if err := connection.DB.Create(&relation).Error; err != nil {
+		return err
+	}
+
+	if err := connection.DB.Create(&debitur).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UpdateRekeningDebitur(id string, updatedRekeningDebitur models.DataRekeningDebitur) (result models.DataRekeningDebitur, err error) {
+	var rekeningDebitur models.DataRekeningDebitur
+	if err := connection.DB.First(&rekeningDebitur, id).Error; err != nil {
+		return result, errors.New("data Not Found")
+	}
+
+	rekeningDebitur.Name = updatedRekeningDebitur.Name
+	rekeningDebitur.NoIdCard = updatedRekeningDebitur.NoIdCard
+	rekeningDebitur.NPWP = updatedRekeningDebitur.NPWP
+	rekeningDebitur.KeyPerson = updatedRekeningDebitur.KeyPerson
+	rekeningDebitur.NoRekening = updatedRekeningDebitur.NoRekening
+	rekeningDebitur.Remark = updatedRekeningDebitur.Remark
+
+	if err := connection.DB.Save(&rekeningDebitur).Error; err != nil {
+		return result, errors.New("failed to update the user data")
+	}
+
+	return rekeningDebitur, nil
+}
+
+func UpdateRelationWithBank(id string, updateRelationBank models.RelationWithBank) (result models.RelationWithBank, err error) {
+	var relationBank models.RelationWithBank
+	if err := connection.DB.First(&relationBank, id).Error; err != nil {
+		return result, errors.New("data Not Found")
+	}
+
+	relationBank.Giro = updateRelationBank.Giro
+	relationBank.Tabungan = updateRelationBank.Tabungan
+	relationBank.NoRekening = updateRelationBank.NoRekening
+	relationBank.Debitur = updateRelationBank.Debitur
+
+	if err := connection.DB.Save(&relationBank).Error; err != nil {
+		return result, errors.New("failed to update the user data")
+	}
+
+	return relationBank, nil
+}
+
+func DeleteRelationWithBank(Id string) (result models.RelationWithBank, err error) {
+	var relation models.RelationWithBank
+	if err := connection.DB.First(&relation, Id).Error; err != nil {
+		return result, errors.New("data Not Found")
+	}
+
+	relation.Status = "D"
+
+	if err := connection.DB.Save(&relation).Error; err != nil {
+		return result, errors.New("failed to delete")
+	}
+
+	return relation, nil
+}
+
+func DeleteRekeningDebitur(Id string) (result models.DataRekeningDebitur, err error) {
+	var rekeningDebitur models.DataRekeningDebitur
+	if err := connection.DB.First(&rekeningDebitur, Id).Error; err != nil {
+		return result, errors.New("Data Not Found")
+	}
+
+	rekeningDebitur.Status = "D"
+
+	if err := connection.DB.Save(&rekeningDebitur).Error; err != nil {
+		return result, errors.New("Failed to delete")
+	}
+
+	return rekeningDebitur, nil
+}
+
+func ShowRelationWithBank() (result []models.RelationWithBank) {
+	var relationBank []models.RelationWithBank
+
+	connection.DB.Where("status = ?", "L").Find(&relationBank)
+
+	return relationBank
+}
+
+func ShowRekeningDebitur() (result []models.OwnershipData) {
+	var rekeningDebitur []models.OwnershipData
+
+	connection.DB.Where("status = ?", "L").Find(&rekeningDebitur)
+
+	return rekeningDebitur
+}
