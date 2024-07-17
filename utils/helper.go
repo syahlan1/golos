74bb920585b8d0ffe53b5d2a5fff2197b4674184
chico <chico.jagul@gmail.com>
2024-07-17 13:50:58 +0700

    fix checkSQL

--- utils/helper.go
+++ utils/helper.go
@@ -320,21 +320,21 @@ func Decrypt(encryptedText string) (string, error) {
 }
 
 func IsValidSqlName(name string) error {
-    match, _ := regexp.MatchString(`^[a-zA-Z_][a-zA-Z0-9_]*$`, name)
-    if !match {
-        return errors.New("invalid field_name")
-    }
+	match, _ := regexp.MatchString(`^[a-zA-Z_][a-zA-Z0-9_]*$`, name)
+	if !match {
+		return errors.New("invalid field_name")
+	}
 
 	if len(name) > 63 {
 		return errors.New("name too long")
 	}
-    return nil
+	return nil
 }
 
 func IsValidSQL(query string) error {
-    match, _ := regexp.MatchString(`^[a-zA-Z0-9_ ,.*()]+$`, query)
-    if !match {
-        return errors.New("invalid characters in query")
-    }
-    return nil
+	match, _ := regexp.MatchString("(?i)\\b(INSERT|UPDATE|DELETE|DROP|CREATE|ALTER|TRUNCATE|GRANT|REVOKE|EXEC|INTO)\\b", query)
+	if match {
+		return errors.New("invalid characters in query")
+	}
+	return nil
 }
